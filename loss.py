ff98c78c19520e4aed9a7b50f177a117d17adaf8
TornikeAm <82511782+TornikeAm@users.noreply.github.com>
2021-08-29 09:29:02 +0400

    Add files via upload

---
+++ loss.py
@@ -0,0 +1,21 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class FocalLoss(nn.Module):
+    def __init__(self, alpha=1, gamma=2, reduce=True):
+        super(FocalLoss, self).__init__()
+        self.alpha = alpha
+        self.gamma = gamma
+        self.reduce = reduce
+
+    def forward(self, inputs, targets):
+        ce = F.cross_entropy(inputs, targets, reduction='none')
+        p = torch.exp(-ce)
+        loss = self.alpha * (1 - p) ** self.gamma * ce
+
+        if self.reduce:
+            return torch.mean(loss)
+        return loss
+        
